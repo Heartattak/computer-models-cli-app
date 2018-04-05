8e507046c7bad3c4f44a94a5d6502a8562e6870f
Brent  Ransom <sadumedusa@gmail.com>
2018-04-05 13:19:49 +0000

    updated array

--- lib/computer_models_cli_app/Scraper.rb
+++ lib/computer_models_cli_app/Scraper.rb
@@ -4,7 +4,7 @@ class ComputerModelsCliApp::Scraper
     doc = Nokogiri::HTML(open("http://www.tigerdirect.com/applications/Category/guidedSearch.asp?CatId=17"))
     array = []
     doc.css("div.product").each do |model|
-      name = model.css("itemName a").text
+      name = model.css(".itemName a").text
       price = model.css("div.product.productAction.salePrice sup").text.to_i
       url = model.css("div.product.productInfo.itemName a")
 
