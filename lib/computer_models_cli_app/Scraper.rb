36ced8655deb0710d08687938ba8e6f804f0611a
Brent  Ransom <sadumedusa@gmail.com>
2018-04-26 09:58:31 +0000

    updated url scraper

--- lib/computer_models_cli_app/Scraper.rb
+++ lib/computer_models_cli_app/Scraper.rb
@@ -7,7 +7,7 @@ class ComputerModelsCliApp::Scraper
       computer = ComputerModelsCliApp::Computers.new
       computer.name = model.css(".itemName a").text
       computer.price = "$" + model.css(".salePrice").children[4].text + model.css(".salePrice").children[5].text
-      computer.url = model.css(".itemName a").attribute("href")
+      computer.url = "http://www.tigerdirect.com" + model.css(".itemName a").attribute("href").text.gsub("..", "")
 
       computer.save
     end
